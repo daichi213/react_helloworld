85f2de0d76c6d67dd0cde138ad240706da6cf6e9
administrator <admin@administrators-MacBook-Pro.local>
2020-12-01 15:55:12 +0900

    functional component class component 学習

--- workplace/src/App.js
+++ workplace/src/App.js
@@ -1,9 +1,31 @@
-import React, { Component } from 'react';
+// JSXを使用するためのメソッド
+// import React, { Component } from 'react';
+import React from 'react';
+// class component
+// class App extends React.Component {
+//   render(){
+//     const greeting = "Hi, Tom!";
+//     const dom = <h1 chassName="foo">{greeting}</h1>;
+//     // returnにはひとつの要素のみしか出力できない
+//     return dom;
+//   }
+// }
 
-class App extends Component {
-  render(){
-    return <h1>Hello World!</h1>;
-  }
+// functional component
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
+}
+
+const Cat = () => {
+  return <div>Meow!!</div>
 }
 
 export default App;
