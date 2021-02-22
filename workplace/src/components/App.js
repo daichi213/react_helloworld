916ccab8af78eb35508a2cbfb9abdbbc68dabc00
administrator <admin@administrators-MacBook-Pro.local>
2021-02-22 16:30:57 +0900

    implement connection with connect function

--- workplace/src/components/App.js
+++ workplace/src/components/App.js
@@ -1,42 +1,31 @@
 // JSXを使用するためのメソッド
 import React, { Component } from 'react';
-// functional component
-const App = () => {
-  return (
-    <Counter></Counter>
-  )
-}
+import { connect } from 'react-redux';
+
+import { increment, decrement } from '../actions'
 
 // 中括弧部分をpropsという
-class Counter extends Component {
-  constructor(props) {
-    super(props)
-    console.log(this.state)
-    this.state = {count: 0}
-  }
-
-  handlePlusButton = () => {
-    // setStateが実行されるとrenderが毎度実行される。
-    this.setState({count: this.state.count + 1})
-  }
-
-  handleMinusButton = () => {
-    this.setState({count: this.state.count - 1})
-  }
-
+class App extends Component {
   render(){
-    // setState実行時に同時に実行される
-    console.log("render")
+    const props = this.props
     return (
       // JSX内で使用するスーパークラスは1つでなければ例外が返される。
       // そのため、以下のようにFragmentで囲むことでこの例外は回避できる
       <React.Fragment>
-        <div>counter: {this.state.count}</div>
-        <button onClick={this.handlePlusButton}>+1</button>
-        <button onClick={this.handleMinusButton}>-1</button>
+        <div>value: {props.value}</div>
+        <button onClick={props.increment}>+1</button>
+        <button onClick={props.decrement}>-1</button>
       </React.Fragment>
     )
   }
 }
 
-export default App;
+const mapStateToProps = state => ({value: state.count.value})
+const mapDispatchToProps = dispatch => ({
+  increment: () => dispatch(increment()),
+  decrement: () => dispatch(decrement())
+})
+// 略式記述
+// const mapDispatchToProps = ({ increment, decrement})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
